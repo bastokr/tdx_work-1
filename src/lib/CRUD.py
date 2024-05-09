6850b242a68ccc45770ddacda37a741e4342e56b
hyojun kim <37866182+gimyojun@users.noreply.github.com>
2024-05-09 17:17:39 +0900

    feat: 쿼리 테스트 로직 구현

--- src/lib/CRUD.py
+++ src/lib/CRUD.py
@@ -1,16 +1,17 @@
 from lib.Databases import Databases
 import psycopg2
 import psycopg2.extras  # psycopg2.extras 모듈을 임포트
+
 class CRUD(Databases):
-    def insertDB(self,  table, colum, data):
-        sql = "INSERT INTO {} ({}) VALUES %s;".format( table, colum)
+    def insertDB(self, table, colum, data):
+        sql = "INSERT INTO {} ({}) VALUES %s;".format(table, colum)
         try:
             self.cursor.execute(sql, (data,))
             self.db.commit()
         except Exception as e:
             print("Insert DB error:", e)
-    
-    def insertDBSeq(self, table, colum, data,seq):
+
+    def insertDBSeq(self, table, colum, data, seq):
         sql = "INSERT INTO {} ({}) VALUES %s;".format(table, colum)
         try:
             self.cursor.execute(sql, (data,))
@@ -27,15 +28,15 @@ class CRUD(Databases):
             print(f"Read DB error: {e}")
             raise e
 
-    def whereDB(self,  table, colum ,where):
-        sql = "SELECT {} FROM  {} WHERE {} ".format(colum,   table , where)
+    def whereDB(self, table, colum, where):
+        sql = "SELECT {} FROM  {} WHERE {}".format(colum, table, where)
         try:
             self.cursor.execute(sql)
             result = self.cursor.fetchall()
             return result
         except Exception as e:
-            print("Read DB error:", e)  
-                    
+            print("Read DB error:", e)
+
     def updateDB(self, schema, table, colum, value, condition):
         sql = "UPDATE {}.{} SET {}=%s WHERE {}=%s".format(schema, table, colum, colum)
         try:
@@ -52,9 +53,31 @@ class CRUD(Databases):
         except Exception as e:
             print("Delete DB error:", e)
 
+    def execute_param_query(self, query, params):
+        """
+        파라미터화된 쿼리를 실행하고 결과를 반환하는 메서드
+        :param query: 실행할 쿼리 문자열
+        :param params: 파라미터로 대체할 값들이 담긴 딕셔너리
+        :return: 쿼리 결과
+        """
+        try:
+            # ':' 파라미터를 실제 값으로 대체
+            formatted_params = {key.lstrip(':'): value for key, value in params.items()}
+            for key, value in formatted_params.items():
+                query = query.replace(f":{key}", f"{{{key}}}")
+
+            # `params` 딕셔너리의 키를 `{}`로 둘러싸인 플레이스홀더에 맵핑하여 대체
+            query = query.format(**formatted_params)
+
+            print("Executing Query:", query)  # 쿼리 확인용 로그
+
+            self.cursor.execute(query)
+            return self.cursor.fetchall()
+        except Exception as e:
+            print("Query execution with parameters error:", e)
+            return []
+
 if __name__ == "__main__":
     db = CRUD()
-    db.insertDB(schema='myschema', table='table', colum='ID', data='유동적변경')
-    print(db.readDB(schema='myschema', table='table', colum='ID'))
-    db.updateDB(schema='myschema', table='table', colum='ID', value='와우', condition='유동적변경')
-    db.deleteDB(schema='myschema', table='table', condition="id != 'd'")
+    print(db.readDB(table='public.my_table', colum='*'))
+    db.updateDB(schema='public', table='my_table', colum='name', value='new_value', condition="id = 1")
